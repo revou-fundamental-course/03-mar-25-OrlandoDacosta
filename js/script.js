23f10e49be443997428d3c22111cfb89fef2129c
Orlando Dacosta <orlandodacosta31@gmail.com>
2025-03-10 21:12:27 +0700

    update Alert Welcome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Ini file javascript
+function replaceName() {
+  let name = prompt("Masukkan Nama Anda?", "");
+  document.getElementById("name").innerHTML = name;
+}
+
+replaceName();
+
+document.getElementById("tombol").addEventListener("click", function () {
+  replaceName();
+});
