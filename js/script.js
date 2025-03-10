68f8a901ae007d053745a326f847c424798f54f1
Orlando Dacosta <orlandodacosta31@gmail.com>
2025-03-10 22:25:23 +0700

    update script content messages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 // Ini file javascript
 function replaceName() {
   let name = prompt("Masukkan Nama Anda?", "");
-  document.getElementById("name").innerHTML = name;
+  document.getElementById("name-user").innerHTML = name;
 }
 
 replaceName();
 
-document.getElementById("tombol").addEventListener("click", function () {
-  replaceName();
-});
+// document.getElementById("tombol").addEventListener("click", function () {
+//   replaceName();
+// });
+
+function setSenderUI(name, birthDate, gender, messages) {
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthDate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
+}
+
+function validateForm() {
+  const name = document.forms["message-form"]["full-name"].value;
+  const birthDate = document.forms["message-form"]["birth-date"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const messages = document.forms["message-form"]["messages"].value;
+
+  if (name == "" || birthDate == "" || gender == "" || messages == "") {
+    alert("Semua field harus diisi!");
+    return false;
+  }
+
+  setSenderUI(name, birthDate, gender, messages);
+
+  return false;
+}
